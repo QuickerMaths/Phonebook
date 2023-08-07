8dc4f69122985e3e8f3b79dff9838fe2fcfbbfe6
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-08-07 20:42:47 +0200

    changed App to class component and added nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <></>;
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  render() {
+    return (
+      <main className="App">
+        <h1>Contacts</h1>
+      </main>
+    );
+  }
 }
 
 export default App;
