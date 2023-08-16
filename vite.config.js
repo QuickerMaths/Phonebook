fa28fccaf28657ce01766ed03e35d2817cdb5c52
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-08-16 20:35:50 +0200

    changed repo name for proper deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-react-hw-06-phonebook/",
+  base: "/goit-react-hw-07-phonebook/",
   plugins: [react()],
 });
